35498da390ee163e5278a72a0362a0ad13978743
Kevin Schaul <kevin.schaul@gmail.com>
2013-10-22 22:41:39 -0500

    Added Toasts to buttons

--- src/com/csci5115/group2/planmymeal/HomeActivity.java
+++ src/com/csci5115/group2/planmymeal/HomeActivity.java
@@ -1,8 +1,11 @@
 package com.csci5115.group2.planmymeal;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
 
 public class HomeActivity extends Activity {
 
@@ -18,6 +21,42 @@ public class HomeActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.home, menu);
         return true;
+    }
+    
+    public void openSettings(View view) {
+    	Context context = getApplicationContext();
+    	CharSequence text = "Settings is not yet implemented.";
+    	int duration = Toast.LENGTH_SHORT;
+    	
+    	Toast toast = Toast.makeText(context, text, duration);
+    	toast.show();
+    }
+    
+    public void openCookbook(View view) {
+    	Context context = getApplicationContext();
+    	CharSequence text = "Community Cookbook is not yet implemented.";
+    	int duration = Toast.LENGTH_SHORT;
+    	
+    	Toast toast = Toast.makeText(context, text, duration);
+    	toast.show();
+    }
+    
+    public void newRecipe(View view) {
+    	Context context = getApplicationContext();
+    	CharSequence text = "New recipe is not yet implemented.";
+    	int duration = Toast.LENGTH_SHORT;
+    	
+    	Toast toast = Toast.makeText(context, text, duration);
+    	toast.show();
+    }
+    
+    public void newMeal(View view) {
+    	Context context = getApplicationContext();
+    	CharSequence text = "New meal is not yet implemented.";
+    	int duration = Toast.LENGTH_SHORT;
+    	
+    	Toast toast = Toast.makeText(context, text, duration);
+    	toast.show();
     }
     
 }
