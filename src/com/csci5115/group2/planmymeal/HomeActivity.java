8bb29c94d305813f232b0bce675f1375eb5ea6a9
Kevin Schaul <kevin.schaul@gmail.com>
2013-10-22 23:11:55 -0500

    Use search soft keyboard

--- src/com/csci5115/group2/planmymeal/HomeActivity.java
+++ src/com/csci5115/group2/planmymeal/HomeActivity.java
@@ -3,25 +3,41 @@ package com.csci5115.group2.planmymeal;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
-import android.view.Menu;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
 import android.widget.Toast;
 
-public class HomeActivity extends Activity {
+
+public class HomeActivity extends Activity implements OnEditorActionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        
+        TextView search = (TextView) findViewById(R.id.search);
+		search.setOnEditorActionListener(this);
     }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.home, menu);
-        return true;
-    }
+    
+	@Override
+	public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+		boolean handled = false;
+		Log.i("onEditorAction()", Integer.toString(actionId));
+		if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+	    	Context context = getApplicationContext();
+	    	CharSequence text = v.getText();
+	    	int duration = Toast.LENGTH_SHORT;
+	    	
+	    	Toast toast = Toast.makeText(context, text, duration);
+	    	toast.show();
+		}
+		
+		return handled;
+	}
     
     public void openSettings(View view) {
     	Context context = getApplicationContext();
