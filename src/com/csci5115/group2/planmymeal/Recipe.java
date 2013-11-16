4a2d9f69e49ac2662987c395990a6f8c646342d3
DSOL <Solom146@umn.edu>
2013-11-16 15:44:01 -0600

    Adding Recipe.java

---
+++ src/com/csci5115/group2/planmymeal/Recipe.java
@@ -0,0 +1,14 @@
+package com.csci5115.group2.planmymeal;
+
+public class Recipe {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
