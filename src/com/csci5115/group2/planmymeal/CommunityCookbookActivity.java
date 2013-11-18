ba7056636cf8dcd3340258792ce18890aaffc903
Kevin Schaul <kevin.schaul@gmail.com>
2013-11-17 22:12:52 -0600

    Add community cookbook array adapter

--- src/com/csci5115/group2/planmymeal/CommunityCookbookActivity.java
+++ src/com/csci5115/group2/planmymeal/CommunityCookbookActivity.java
@@ -1,8 +1,13 @@
 package com.csci5115.group2.planmymeal;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class CommunityCookbookActivity extends Activity {
 
@@ -10,6 +15,31 @@ public class CommunityCookbookActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_community_cookbook);
+		
+		// Set up list view
+		// TODO Use a database call to populate this list view
+		Meal[] mealArray = new Meal[10];
+		
+		for (int i = 0; i < 10; i++) {
+			if (i % 2 == 0) {
+				mealArray[i] = new Meal("Beef Stew", "1:18:00");
+			} else {
+				mealArray[i] = new Meal("Olivia's Favorite", "1:40:22");
+			}
+		}
+
+		CommunityCookbookArrayAdapter communityCookbookAdapter = new CommunityCookbookArrayAdapter(this, mealArray);
+		ListView listView = (ListView) findViewById(R.id.communityCookbook_mealListView);
+		listView.setAdapter(communityCookbookAdapter);
+		listView.setOnItemClickListener(new OnItemClickListener() {
+			  @Override
+			  public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				  Meal meal = (Meal) parent.getItemAtPosition(position);
+			    Toast.makeText(getApplicationContext(),
+			      meal.getName(), Toast.LENGTH_LONG)
+			      .show();
+			  }
+			}); 
 	}
 
 	@Override
