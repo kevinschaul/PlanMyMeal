a447975fc04ce04040ace25c5143234afdb01ccc
Kevin Schaul <kevin.schaul@gmail.com>
2013-11-29 12:44:07 -0600

    Meal detail styles

--- src/com/csci5115/group2/planmymeal/RecipeDetailArrayAdapter.java
+++ src/com/csci5115/group2/planmymeal/RecipeDetailArrayAdapter.java
@@ -3,6 +3,7 @@ package com.csci5115.group2.planmymeal;
 import java.util.List;
 
 import android.content.Context;
+import android.graphics.Typeface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,11 +14,13 @@ public class RecipeDetailArrayAdapter extends ArrayAdapter<Recipe> {
 	
 	private final Context context;
 	private final List<Recipe> recipes;
+	private final Typeface fontAwesome;
 
 	public RecipeDetailArrayAdapter(Context context, List<Recipe> recipes) {
 		super(context, R.layout.row_recipe_detail_array, recipes);
 		this.context = context;
 		this.recipes = recipes;
+		this.fontAwesome = Typeface.createFromAsset(context.getAssets(), "fontawesome-webfont.ttf" );
 	}
 	
 	@Override
@@ -28,11 +31,14 @@ public class RecipeDetailArrayAdapter extends ArrayAdapter<Recipe> {
 	    Recipe recipe = recipes.get(position);
 	    
 	    View rowView = inflater.inflate(R.layout.row_recipe_detail_array, parent, false);
+	    TextView mealType = (TextView) rowView.findViewById(R.id.row_recipe_detail_array_recipeType);
 	    TextView mealName = (TextView) rowView.findViewById(R.id.row_recipe_detail_array_recipeName);
 	    TextView mealTime = (TextView) rowView.findViewById(R.id.row_recipe_detail_array_time);
 	    
 	    if (recipe != null) {
-		    mealName.setText("[" + recipe.getType() + "] " + recipe.getName());
+		    mealType.setText(context.getString(recipe.getTypeIconResource()));
+	    	mealType.setTypeface(this.fontAwesome);
+		    mealName.setText(recipe.getName());
 		    mealTime.setText(recipe.getReadableTime());
 	    }
 	    
