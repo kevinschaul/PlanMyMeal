1cdaa0633d72cb0ebd02ae1d84478d38d06bd039
unknown <willhallwho@gmail.com>
2013-11-30 18:05:50 -0600

    dding in changes to the cook activity page

--- src/com/csci5115/group2/planmymeal/CookActivity.java
+++ src/com/csci5115/group2/planmymeal/CookActivity.java
@@ -5,15 +5,22 @@ import java.util.List;
 
 import com.csci5115.group2.planmymeal.database.DataSourceManager;
 
+import android.R.string;
 import android.app.ActionBar.LayoutParams;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Pair;
 import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.GridView;
+import android.widget.HorizontalScrollView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
@@ -22,10 +29,11 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
-public class CookActivity extends Activity {
+public class CookActivity extends Activity implements OnClickListener {
 	
 	// Databases
 	private DataSourceManager datasource;
+	public HorizontalScrollView allRecipes;
 	
 	public LinearLayout recipeHolder0;
 	public LinearLayout recipeHolder1;
@@ -35,15 +43,28 @@ public class CookActivity extends Activity {
 	public Recipe recipe1;
 	public Recipe recipe2;
 	public Recipe recipe3;
+	public LinkedList<Recipe> allRecipesList;
+	RecipeStepsArrayAdapter recipeStepAdapter0;
+	RecipeStepsArrayAdapter recipeStepAdapter1;
+	RecipeStepsArrayAdapter recipeStepAdapter2;
+	RecipeStepsArrayAdapter recipeStepAdapter3;
+	
+	public RecipeStep currentStep;
+	public TextView currentStepDescription;
+	public TextView currentStepTime;
+	public int currentRecipe;
+	public Button nextStep;
+	
+	public LinkedList<string> StepOrder;
 	
 	public int numRecipes;
-	public RelativeLayout currentRecipe;
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_cook);
-		
+		allRecipes = (HorizontalScrollView) findViewById(R.id.myListView);
 		Intent intent = getIntent();
 		Long mealId = intent.getLongExtra(HomeActivity.EXTRA_MEAL, 0);
 		
@@ -53,19 +74,12 @@ public class CookActivity extends Activity {
 		
         numRecipes = 4;
         
-        Meal meal = datasource.getMealById(mealId);
+        //Meal meal = datasource.getMealById(mealId);
 		
-		List<Recipe> recipes = datasource.getMealRecipes(mealId);// meal.getRecipes();
+		//List<Recipe> recipes = datasource.getMealRecipes(mealId);// meal.getRecipes();
 		
-		CreateFakeRecipes();
-		 
-		recipe0 = recipes.get(0);
-		recipe0.setIngredients(datasource.getRecipeIngredients(recipe0.id));
-		recipe0.setSteps(datasource.getRecipeSteps(recipe0.id));
-		//Recipe secondRecipe = recipes.get(1); //new Recipe();
-		//secondRecipe.setIngredients(datasource.getRecipeIngredients(secondRecipe.id));
-		//secondRecipe.setSteps(datasource.getRecipeSteps(secondRecipe.id));
-		
+		recipe0 = new Recipe();
+		CreateFakeRecipes();	
 		
 		
 		recipeHolder0 = (LinearLayout)findViewById(R.id.RecipeHolder0);
@@ -79,8 +93,17 @@ public class CookActivity extends Activity {
 		//removeRecipeStep(0);
 		removeRecipeStep(1);
 		
+		
+		currentStepDescription = (TextView)findViewById(R.id.currentStepDescription);
+		currentStepTime = (TextView)findViewById(R.id.currentStepTime);
+		nextStep = (Button)findViewById(R.id.button0);
+		nextStep.setOnClickListener(this);	
+		
+		
+		setCurrentStep();	
 	}
 	
+	 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -116,48 +139,159 @@ public class CookActivity extends Activity {
 				newSteps = recipe0.getSteps();
 				newSteps.remove(0);
 				recipe0.setSteps(newSteps);
+				allRecipes.refreshDrawableState();
+	        	recipeStepAdapter0.notifyDataSetChanged();
+	        	recipeHolder0.refreshDrawableState();
 				break;
 			case 1:
 				newSteps = recipe1.getSteps();
 				newSteps.remove(0);
 				recipe1.setSteps(newSteps);
+				allRecipes.refreshDrawableState();
+	        	recipeStepAdapter1.notifyDataSetChanged();
+	        	recipeHolder1.refreshDrawableState();
 				break;
 			case 2:
 				newSteps = recipe2.getSteps();
 				newSteps.remove(0);
 				recipe2.setSteps(newSteps);
+				allRecipes.refreshDrawableState();
+	        	recipeStepAdapter2.notifyDataSetChanged();
+	        	recipeHolder2.refreshDrawableState();
 				break;
 			case 3:
 				newSteps = recipe3.getSteps();
 				newSteps.remove(0);
 				recipe3.setSteps(newSteps);
+				allRecipes.refreshDrawableState();
+	        	recipeStepAdapter3.notifyDataSetChanged();
+	        	recipeHolder3.refreshDrawableState();
 				break;
 		}
 	}
 	
 	
 	public void populateRecipes(){
+		allRecipesList = new LinkedList<Recipe>();
+
+		
 		switch(numRecipes){
 			case 1:
 				populateFirstRecipe();
+				allRecipesList.add(recipe0);
 				break;
 			case 2:
 				populateFirstRecipe();
 				populateSecondRecipe();
+				allRecipesList.add(recipe0);
+				allRecipesList.add(recipe1);
 				break;
 			case 3:
 				populateFirstRecipe();
 				populateSecondRecipe();
 				populateThirdRecipe();
+				allRecipesList.add(recipe0);
+				allRecipesList.add(recipe1);
+				allRecipesList.add(recipe2);
 				break;
 			case 4:
 				populateFirstRecipe();
 				populateSecondRecipe();
 				populateThirdRecipe();
 				populateFourthRecipe();
+				allRecipesList.add(recipe0);
+				allRecipesList.add(recipe1);
+				allRecipesList.add(recipe2);
+				allRecipesList.add(recipe3);
 				break;
 		}
 	}
+	
+	public void hideRecipe(int currentRecipe){
+		switch(currentRecipe){
+		case 1:
+			recipeHolder0.setVisibility(View.GONE);
+			break;
+		case 2:
+			recipeHolder1.setVisibility(View.GONE);
+			break;
+		case 3:
+			recipeHolder2.setVisibility(View.GONE);
+			break;
+		case 4:
+			recipeHolder3.setVisibility(View.GONE);
+			break;
+	}
+		
+	}
+	
+	@Override
+	public void onClick(View v) {
+			Context context = v.getContext();
+			Intent intent;
+			
+		    switch (v.getId()) {
+		        case R.id.button0:
+		        	//populateSecondRecipe();
+		        	removeRecipeStep(currentRecipe);
+		        	setCurrentStep();
+		    		break;
+		        default:
+		            return;
+		    }
+	}
+	
+	
+	public void setCurrentStep(){
+		RecipeStep dummyStep = new RecipeStep();
+		dummyStep.setTime(-1);
+		RecipeStep curStep = new RecipeStep();
+		RecipeStep MaxStep = dummyStep;
+		int recipe = 0;
+		
+		for(Recipe r : allRecipesList){
+			
+			if(!r.getSteps().isEmpty()){	
+				curStep = r.getSteps().get(0);
+				if(MaxStep.getTime() <= curStep.getTime()){
+					MaxStep = curStep;
+					currentRecipe = recipe;
+				}
+			}
+			
+			recipe++;
+		}
+		
+		currentStep = MaxStep;
+		currentStepDescription.setText(currentStep.getInstructions());
+		currentStepTime.setText(Long.toString(currentStep.getTime()));
+		currentStepDescription.refreshDrawableState();
+		currentStepTime.refreshDrawableState();
+		
+		new CountDownTimer(currentStep.getTime()*60000, 1000) {
+
+		     public void onTick(long millisUntilFinished) {
+		         currentStepTime.setText(makeTimeString(millisUntilFinished));
+		     }
+
+		     public void onFinish() {
+		         currentStepTime.setText("done!");
+		         nextStep.callOnClick();
+		     }
+		  }.start();
+		
+	}
+	
+	public String makeTimeString(long millis){
+		long second = (millis / 1000) % 60;
+		long minute = (millis / (1000 * 60)) % 60;
+		long hour = (millis / (1000 * 60 * 60)) % 24;
+
+		String time = String.format("%02d:%02d:%02d", hour, minute, second);
+		
+		return time;
+	}
+	
 	
 	public void populateFirstRecipe(){
 		TextView myText0 = new TextView(this);
@@ -168,7 +302,7 @@ public class CookActivity extends Activity {
 		recipeIngredients0.setNumColumns(2);
 		recipeIngredients0.setLayoutParams(cookIngredients.getLayoutParams());
 		recipeIngredients0.setAdapter(ingredientsAdapter0);
-		RecipeStepsArrayAdapter recipeStepAdapter0 = new RecipeStepsArrayAdapter(this, (LinkedList)recipe0.getSteps());
+		recipeStepAdapter0 = new RecipeStepsArrayAdapter(this, (LinkedList)recipe0.getSteps());
 		ListView recipeStepsListView0 = new ListView(this);
 		ListView cookSteps =(ListView) findViewById(R.id.recipeCookSteps);
 		recipeStepsListView0.setLayoutParams(cookSteps.getLayoutParams());
@@ -177,6 +311,15 @@ public class CookActivity extends Activity {
 		recipeHolder0.addView(myText0);
 		recipeHolder0.addView(recipeIngredients0);
 		recipeHolder0.addView(recipeStepsListView0);
+	}
+	
+	public void calculateRecipeTime(Recipe recipe){
+		List<RecipeStep> steps = recipe.getSteps();
+		long totalTime = 0;
+		for(RecipeStep r : steps){
+			totalTime += r.getTime();
+		}
+		recipe.setTime(totalTime);
 	}
 	
 	public void populateSecondRecipe(){
@@ -188,7 +331,7 @@ public class CookActivity extends Activity {
 		recipeIngredients1.setNumColumns(2);
 		recipeIngredients1.setLayoutParams(cookIngredients.getLayoutParams());
 		recipeIngredients1.setAdapter(ingredientsAdapter1);
-		RecipeStepsArrayAdapter recipeStepAdapter1= new RecipeStepsArrayAdapter(this, (LinkedList)recipe1.getSteps());
+		recipeStepAdapter1= new RecipeStepsArrayAdapter(this, (LinkedList)recipe1.getSteps());
 		ListView recipeStepsListView1 = new ListView(this);
 		ListView cookSteps =(ListView) findViewById(R.id.recipeCookSteps);
 		recipeStepsListView1.setLayoutParams(cookSteps.getLayoutParams());
@@ -198,6 +341,10 @@ public class CookActivity extends Activity {
 		recipeHolder1.addView(recipeIngredients1);
 		recipeHolder1.addView(recipeStepsListView1);
 		recipeHolder1.setVisibility(View.VISIBLE);
+		calculateRecipeTime(recipe0);
+		calculateRecipeTime(recipe1);
+		calculateRecipeTime(recipe2);
+		calculateRecipeTime(recipe3);
 	}
 
 	public void populateThirdRecipe(){
@@ -209,7 +356,7 @@ public class CookActivity extends Activity {
 		recipeIngredients2.setNumColumns(2);
 		recipeIngredients2.setLayoutParams(cookIngredients.getLayoutParams());
 		recipeIngredients2.setAdapter(ingredientsAdapter2);
-		RecipeStepsArrayAdapter recipeStepAdapter2 = new RecipeStepsArrayAdapter(this, (LinkedList)recipe2.getSteps());
+		recipeStepAdapter2 = new RecipeStepsArrayAdapter(this, (LinkedList)recipe2.getSteps());
 		ListView recipeStepsListView2 = new ListView(this);
 		ListView cookSteps =(ListView) findViewById(R.id.recipeCookSteps);
 		recipeStepsListView2.setLayoutParams(cookSteps.getLayoutParams());
@@ -230,7 +377,7 @@ public class CookActivity extends Activity {
 		recipeIngredients3.setNumColumns(2);
 		recipeIngredients3.setLayoutParams(cookIngredients.getLayoutParams());
 		recipeIngredients3.setAdapter(ingredientsAdapter3);
-		RecipeStepsArrayAdapter recipeStepAdapter3 = new RecipeStepsArrayAdapter(this, (LinkedList)recipe3.getSteps());
+		recipeStepAdapter3 = new RecipeStepsArrayAdapter(this, (LinkedList)recipe3.getSteps());
 		ListView recipeStepsListView3 = new ListView(this);
 		ListView cookSteps =(ListView) findViewById(R.id.recipeCookSteps);
 		recipeStepsListView3.setLayoutParams(cookSteps.getLayoutParams());
@@ -244,6 +391,10 @@ public class CookActivity extends Activity {
 	
 	public void CreateFakeRecipes(){
 		
+		
+		LinkedList<Ingredient> ingredients0 = new LinkedList<Ingredient>();
+		LinkedList<RecipeStep> recipeSteps0 = new LinkedList<RecipeStep>();
+		
 		LinkedList<Ingredient> ingredients1 = new LinkedList<Ingredient>();
 		LinkedList<RecipeStep> recipeSteps1 = new LinkedList<RecipeStep>();
 		
@@ -252,6 +403,38 @@ public class CookActivity extends Activity {
 		
 		LinkedList<Ingredient> ingredients3 = new LinkedList<Ingredient>();
 		LinkedList<RecipeStep> recipeSteps3 = new LinkedList<RecipeStep>();
+		
+		Ingredient brownSugar = new Ingredient("Brown Sugar", 1, "Cup");
+		Ingredient Apples = new Ingredient("Apples", 2, "Whole");
+		Ingredient sugar = new Ingredient("Sugar", 1, "Cup");
+		Ingredient pieCrust = new Ingredient("Pie Crust", 1, "Whole");
+		
+		ingredients0.add(brownSugar);
+		ingredients0.add(Apples);
+		ingredients0.add(sugar);
+		ingredients0.add(pieCrust);
+		
+		RecipeStep o = new RecipeStep();
+		o.setInstructions("Preheat ove to 350F");
+		o.setTime(10);
+		RecipeStep tw = new RecipeStep();
+		tw.setInstructions("Cut up Apples");
+		tw.setTime(5);
+		RecipeStep th = new RecipeStep();
+		th.setInstructions("Mix up all ingredients");
+		th.setTime(3);
+		RecipeStep f = new RecipeStep();
+		f.setInstructions("Bake for 30 minutes");
+		f.setTime(30);
+		
+		recipeSteps0.add(o);
+		recipeSteps0.add(tw);
+		recipeSteps0.add(th);
+		recipeSteps0.add(f);
+		recipe0.setName("Apple Pie");
+		recipe0.setIngredients(ingredients0);
+		recipe0.setSteps(recipeSteps0);
+		
 		
 		Ingredient oliveOil = new Ingredient("Olive Oil", 3, "tblsp");
 		Ingredient sage = new Ingredient("Sage", 1, "Bunch");
@@ -278,24 +461,34 @@ public class CookActivity extends Activity {
 		
 		RecipeStep one = new RecipeStep();
 		one.setInstructions("Set a large saute pan over medium heat and add olive oil, sage and thyme sprigs");
+		one.setTime(10);
 		RecipeStep two = new RecipeStep();
+		two.setTime(1);
 		two.setInstructions("Remove the sage and thyme and set aside on a paper towel to drain");
 		RecipeStep three = new RecipeStep();
 		three.setInstructions("Season with salt and pepper");
+		three.setTime(1);
 		RecipeStep four = new RecipeStep();
 		four.setInstructions("Remove onions from pan and add apples");
+		four.setTime(2);
 		RecipeStep five = new RecipeStep();
 		five.setInstructions("Crush the pecans and add to the pan");
+		five.setTime(5);
 		RecipeStep six = new RecipeStep();
 		six.setInstructions("Add more oil, if needed and season with salt and pepper");
+		six.setTime(2);
 		RecipeStep seven = new RecipeStep();
 		seven.setInstructions("Gently saute until pecans are lightly toasted and apples are just cooked slightly");
+		seven.setTime(10);
 		RecipeStep eight = new RecipeStep();
 		eight.setInstructions("whisk together egg, cream, chicken stock, and salt and pepper, to taste");
+		eight.setTime(5);
 		RecipeStep nine = new RecipeStep();
 		nine.setInstructions("Add torn sourdough, caramelized onions, apples, pecans and chopped parsley");
+		nine.setTime(5);
 		RecipeStep ten = new RecipeStep();
 		ten.setInstructions("Using a wooden spoon, mix the stuffing until well combined");
+		ten.setTime(3);
 		recipeSteps1.add(one);
 		recipeSteps1.add(two);
 		recipeSteps1.add(three);
@@ -341,28 +534,40 @@ public class CookActivity extends Activity {
 		
 		one = new RecipeStep();
 		one.setInstructions("Preheat the oven to 350 degrees F");
+		one.setTime(10);
 		two = new RecipeStep();
 		two.setInstructions("Melt the butter in a small saucepan");
+		two.setTime(5);
 		three = new RecipeStep();
 		three.setInstructions("Add zest and juice of lemon and thyme leaves to butter mixture");
+		three.setTime(3);
 		four = new RecipeStep();
 		four.setInstructions("Take giblets out of Turkey and wash Turkey");
+		four.setTime(5);
 		five = new RecipeStep();
 		five.setInstructions("Clean and dry Turkey");
+		five.setTime(5);
 		six = new RecipeStep();
 		six.setInstructions("Place Turkey in large roasting pan");
+		six.setTime(1);
 		seven = new RecipeStep();
-		seven.setInstructions("Liverally apply Salt and Pepper");
+		seven.setInstructions("Liberally apply Salt and Pepper");
+		seven.setTime(1);
 		eight = new RecipeStep();
 		eight.setInstructions("Stuff the cavity with thyme, halved lemon, quartered onion, and garlic");
+		eight.setTime(3);
 		nine = new RecipeStep();
 		nine.setInstructions("Brush outside of the turkey with butter mixed with salt and pepper");
+		nine.setTime(3);
 		ten = new RecipeStep();
 		ten.setInstructions("Ties the legs of the turkey together with string");
+		ten.setTime(1);
 		RecipeStep eleven = new RecipeStep();
 		eleven.setInstructions("Roast turky for 2 1/2 hours or until juices run clear");
+		eleven.setTime(150);
 		RecipeStep twelve = new RecipeStep();
 		twelve.setInstructions("Remove Turkey and let it rest for 20 minutes before serving");
+		twelve.setTime(20);
 		
 		recipeSteps2.add(one);
 		recipeSteps2.add(two);
@@ -397,10 +602,16 @@ public class CookActivity extends Activity {
 		
 		one = new RecipeStep();
 		one.setInstructions("Stir Soup, Milk, Soy Sauce, Black Pepper, Beans and 2/3 of the Onions in casserole pan");
+		one.setTime(3);
 		two = new RecipeStep();
 		two.setInstructions("Bake at 350 degrees F for 25 minutes");
+		two.setTime(25);
+		three = new RecipeStep();
 		three.setInstructions("Stir Bean mixture and add remaining Onions");
+		three.setTime(3);
+		four = new RecipeStep();
 		four.setInstructions("Bake for 5 more minutes");
+		four.setTime(5);
 		
 		recipeSteps3.add(one);
 		recipeSteps3.add(two);
@@ -411,5 +622,9 @@ public class CookActivity extends Activity {
 		recipe3.setIngredients(ingredients3);
 		recipe3.setSteps(recipeSteps3);
 		
+		calculateRecipeTime(recipe0);
+		calculateRecipeTime(recipe1);
+		calculateRecipeTime(recipe2);
+		calculateRecipeTime(recipe3);
 	}
 }
